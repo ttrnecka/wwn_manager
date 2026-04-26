2f92f905d78bdb2fafa22346400c8c6aabd68970
Tomas Trnecka <trnecka@gmail.com>
2026-04-26 14:57:55 +0200

    Vuln. fixes

--- run_dev_docker_with_build.sh
+++ run_dev_docker_with_build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-touch .env && docker compose -f docker-compose-dev.yaml up -d --build --remove-orphans
+docker compose -f docker-compose-dev.yaml down
+touch .env && docker compose -f docker-compose-dev.yaml up -d --build --remove-orphans 
