90d000af909313a82b6444ded3c2eec403018dcf
Tomas Trnecka <trnecka@gmail.com>
2026-04-30 11:57:26 +0200

    golangci-lint gocyclo 1

--- webapi/internal/handler/fc_entry_handler.go
+++ webapi/internal/handler/fc_entry_handler.go
@@ -21,10 +21,18 @@ import (
 	"github.com/ttrnecka/wwn_identity/webapi/shared/dto"
 )
 
+var wwnRegex = regexp.MustCompile(`^([0-9A-Fa-f]{2}:){7}[0-9A-Fa-f]{2}$`)
+
 type FCWWNEntryHandler struct {
 	service     service.FCWWNEntryService
 	ruleService service.RuleService
 	logger      *zerolog.Logger
+}
+
+type entryLineRecord struct {
+	customer, wwn, zone, alias, loadedHostname string
+	isCsvLoad                                  bool
+	wwnSet                                     int
 }
 
 func NewFCWWNEntryHandler(s service.FCWWNEntryService, r service.RuleService, logger *zerolog.Logger) *FCWWNEntryHandler {
@@ -174,7 +182,7 @@ func (h *FCWWNEntryHandler) ImportHandler(c echo.Context) error {
 }
 
 func (h *FCWWNEntryHandler) ImportAPIHandler(c echo.Context) error {
-	wwnEntries, err := h.readEntriesFromApi(c)
+	wwnEntries, err := h.readEntriesFromAPI(c)
 	if err != nil {
 		return errorWithInternal(http.StatusInternalServerError, "Failed to read entries from api", err)
 	}
@@ -310,12 +318,10 @@ func (h *FCWWNEntryHandler) readEntriesFromFile(file *multipart.FileHeader) ([]*
 	return wwnEntries, nil
 }
 
-func (h *FCWWNEntryHandler) readEntriesFromApi(c echo.Context) ([]*entity.FCWWNEntry, error) {
+func (h *FCWWNEntryHandler) readEntriesFromAPI(c echo.Context) ([]*entity.FCWWNEntry, error) {
 
 	var wwnEntries []*entity.FCWWNEntry
 	wwnEntryMap := make(map[string]map[string]entity.FCWWNEntry, 0)
-
-	re := regexp.MustCompile(`^([0-9A-Fa-f]{2}:){7}[0-9A-Fa-f]{2}$`)
 
 	page := 0
 	pageSize := 10000
@@ -338,60 +344,13 @@ func (h *FCWWNEntryHandler) readEntriesFromApi(c echo.Context) ([]*entity.FCWWNE
 		}
 
 		for _, line := range response.Data.Report.ReportData {
-			wwn, ok := line["wwn"].Value.(string)
-			if !ok {
-				h.logger.Error().Msgf("WWN type is not string: %T", line["wwn"].Value)
-				continue
-			}
-			if !re.MatchString(wwn) {
-				h.logger.Info().Msgf("Invalid WWN: %s", wwn)
+			entryLine, err := h.parseEntryLine(line)
+			if err != nil {
+				h.logger.Error().Msgf("Failed to parse entry line: %v", err)
 				continue
 			}
 
-			customer, ok := line["customer"].Value.(string)
-			if !ok {
-				h.logger.Error().Msgf("customer type is not string: %T", line["customer"].Value)
-				continue
-			}
-			if customer == "" {
-				customer = entity.UnknownCustomer
-			}
-			zone, ok := line["element_name"].Value.(string)
-			if !ok {
-				h.logger.Error().Msgf("zone type is not string: %T", line["element_name"].Value)
-				continue
-			}
-			alias, ok := line["alias"].Value.(string)
-			if !ok {
-				h.logger.Error().Msgf("alias type is not string: %T", line["alias"].Value)
-				continue
-			}
-			loadedHostname, ok := line["loaded_host"].Value.(string)
-			if !ok {
-				h.logger.Error().Msgf("loaded_host type is not string: %T", line["loaded_host"].Value)
-				continue
-			}
-			isCsvLoad := true
-			csvLoad, ok := line["is_csv_load"].Value.(string)
-			if !ok {
-				h.logger.Error().Msgf("is_csv_load type is not string: %T", line["is_csv_load"].Value)
-				continue
-			}
-			if csvLoad == "N" {
-				isCsvLoad = false
-			}
-			wwnSetF, ok := line["wwn_set"].Value.(float64)
-			if !ok {
-				h.logger.Error().Msgf("wwn_set type is not numberic: %T", line["wwn_set"].Value)
-				continue
-			}
-			wwnSet := int(wwnSetF)
-
-			if loadedHostname == "No Matching Rule" {
-				loadedHostname = ""
-			}
-
-			updateEntryMap(wwnEntryMap, customer, wwn, zone, alias, loadedHostname, isCsvLoad, wwnSet)
+			updateEntryMap(wwnEntryMap, entryLine.customer, entryLine.wwn, entryLine.zone, entryLine.alias, entryLine.loadedHostname, entryLine.isCsvLoad, entryLine.wwnSet)
 
 		}
 
@@ -409,6 +368,61 @@ func (h *FCWWNEntryHandler) readEntriesFromApi(c echo.Context) ([]*entity.FCWWNE
 	return wwnEntries, nil
 }
 
+func (h *FCWWNEntryHandler) parseEntryLine(line ita.ReportData) (entryLineRecord, error) {
+	entryLine := entryLineRecord{}
+	wwn, ok := line["wwn"].Value.(string)
+	if !ok {
+		return entryLine, fmt.Errorf("WWN type is not string: %T", line["wwn"].Value)
+	}
+	if !wwnRegex.MatchString(wwn) {
+		return entryLine, fmt.Errorf("invalid WWN: %s", wwn)
+	}
+	entryLine.wwn = wwn
+
+	customer, ok := line["customer"].Value.(string)
+	if !ok {
+		return entryLine, fmt.Errorf("customer type is not string: %T", line["customer"].Value)
+	}
+	if customer == "" {
+		customer = entity.UnknownCustomer
+	}
+	entryLine.customer = customer
+	zone, ok := line["element_name"].Value.(string)
+	if !ok {
+		return entryLine, fmt.Errorf("zone type is not string: %T", line["element_name"].Value)
+	}
+	entryLine.zone = zone
+	alias, ok := line["alias"].Value.(string)
+	if !ok {
+		return entryLine, fmt.Errorf("alias type is not string: %T", line["alias"].Value)
+	}
+	entryLine.alias = alias
+	loadedHostname, ok := line["loaded_host"].Value.(string)
+	if !ok {
+		return entryLine, fmt.Errorf("loaded_host type is not string: %T", line["loaded_host"].Value)
+	}
+	entryLine.loadedHostname = loadedHostname
+	isCsvLoad := true
+	csvLoad, ok := line["is_csv_load"].Value.(string)
+	if !ok {
+		return entryLine, fmt.Errorf("is_csv_load type is not string: %T", line["is_csv_load"].Value)
+	}
+	if csvLoad == "N" {
+		isCsvLoad = false
+	}
+	entryLine.isCsvLoad = isCsvLoad
+	wwnSetF, ok := line["wwn_set"].Value.(float64)
+	if !ok {
+		return entryLine, fmt.Errorf("wwn_set type is not numberic: %T", line["wwn_set"].Value)
+	}
+	wwnSet := int(wwnSetF)
+	entryLine.wwnSet = wwnSet
+
+	if loadedHostname == "No Matching Rule" {
+		entryLine.loadedHostname = ""
+	}
+	return entryLine, nil
+}
 func updateEntryMap(wwnEntryMap map[string]map[string]entity.FCWWNEntry, customer, wwn, zone, alias, loadedHostname string, isCsvLoad bool, wwnSet int) {
 
 	// Create or update FCWWNEntry
